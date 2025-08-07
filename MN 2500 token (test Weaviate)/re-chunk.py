dd24e9e8db2a56b00c5c78f33bda118b1e7dfc72
atl25 <thua72664@gmail.com>
2025-08-07 17:36:33 +0630

    Update re-chunk.py
    nway idea

--- MN 2500 token (test Weaviate)/re-chunk.py
+++ MN 2500 token (test Weaviate)/re-chunk.py
@@ -8,14 +8,14 @@ df = pd.read_csv('MN5_chunk.csv')
 new_rows = []
 
 for idx, row in df.iterrows():
-    chunks = textwrap.wrap(row['pali_text'], width=500, break_long_words=False)
+    chunks = textwrap.wrap(row['pali_text'], width=200, break_long_words=False)
     for i, chunk in enumerate(chunks):
         new_rows.append({
-            'chunk_id': f"{row['chunk_id']}_{i}",
+            'chunk_id': f"{row['chunk_id']}",
+            'sub_chunk_id':f"{row['chunk_id']}_{i}", 
             'pali_text': chunk
         })
 
 # Create new DataFrame
 chunked_df = pd.DataFrame(new_rows)
 chunked_df.to_csv('MN5_re-chunked.csv', index=False)
-
